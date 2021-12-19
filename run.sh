b78ae5d791e9693cdbbac503782ef9f93713cf97
sam mohite <sampanu@gmail.com>
2021-12-19 19:05:18 +0000

    Move the shell script a directory up

---
+++ run.sh
@@ -0,0 +1,21 @@
+##git clone https://github.com/Sampanna/assignment.git	
+## cd assignment
+
+## Build docker image
+docker build -t hello_world ./src/app
+
+## Create Kubernetes objects  
+kubectl apply -f ./src/k8s/
+
+# List pods and services
+kubectl get pods
+kubectl get services
+
+sleep 10
+
+## Get the IP for the service created
+CLUSTER_IP=$(kubectl get svc helloworldservice -ojsonpath='{.spec.clusterIP}')
+
+## Curl the service endpoint
+curl http://$CLUSTER_IP:8080
+
